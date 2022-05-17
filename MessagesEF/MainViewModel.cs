65c5a4e9b72188be45b89fc82e2960a071b7f137
Matthias Druwé <matthias.druwe@gmail.com>
2022-05-17 14:10:23 +0200

    Add project files.

---
+++ MessagesEF/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    internal class MainViewModel
+    {
+        public ObservableCollection<Object> Users { get; set; }
+        public ObservableCollection<Object> Messages { get; set; }
+
+        public Object SelectedUser { get; set; }
+        public Object SelectedMessage { get; set; }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public ActionCommand AddCommand { get; set; }
+        public ActionCommand DeleteCommand { get; set; }
+
+        public MainViewModel()
+        {
+            AddCommand = new ActionCommand(AddCommandAction);
+            DeleteCommand = new ActionCommand(DeleteCommandAction);
+        }
+
+        private void DeleteCommandAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCommandAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
