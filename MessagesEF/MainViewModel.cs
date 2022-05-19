ad75c63c149dc16f180fa93f34da861c7fc50679
Matthias Druwé <matthias.druwe@gmail.com>
2022-05-19 17:17:00 +0200

    Add final solution

--- MessagesEF/MainViewModel.cs
+++ MessagesEF/MainViewModel.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Messages
 {
-    internal class MainViewModel
+    internal class MainViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Object> Users { get; set; }
-        public ObservableCollection<Object> Messages { get; set; }
+        public List<Author> Authors
+        {
+            get => authors;
+            set
+            {
+                authors = value;
+                OnPropertyChanged();
+            }
+        }
+        public List<Post> Posts
+        {
+            get => posts;
+            set
+            {
+                posts = value;
+                OnPropertyChanged();
+            }
+        }
 
-        public Object SelectedUser { get; set; }
-        public Object SelectedMessage { get; set; }
+        public Author SelectedAuthor
+        {
+            get => selectedAuthor;
+            set
+            {
+                selectedAuthor = value;
+                LoadPosts(selectedAuthor);
+            }
+        }
+
+        public Post SelectedPost { get; set; }
 
         public string Title { get; set; }
         public string Content { get; set; }
@@ -21,20 +44,60 @@ namespace Messages
         public ActionCommand AddCommand { get; set; }
         public ActionCommand DeleteCommand { get; set; }
 
+        private Author selectedAuthor;
+        private List<Author> authors;
+        private List<Post> posts;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        AuthorDataRepository authorDataRepository = new AuthorDataRepository();
+        PostDataRepository postDataRepository = new PostDataRepository();
+
         public MainViewModel()
         {
             AddCommand = new ActionCommand(AddCommandAction);
             DeleteCommand = new ActionCommand(DeleteCommandAction);
+            Authors = authorDataRepository.GetAllAuthors();
+        }
+        private void LoadPosts(Author selectedAuthor)
+        {
+            Posts = postDataRepository.GetPostsFor(selectedAuthor);
         }
 
         private void DeleteCommandAction()
         {
-            throw new NotImplementedException();
+            if (SelectedPost != null)
+            {
+                postDataRepository.DeletePost(SelectedPost);
+                LoadPosts(SelectedAuthor);
+            }
         }
 
         private void AddCommandAction()
         {
-            throw new NotImplementedException();
+            if (SelectedAuthor != null)
+            {
+
+                Post post = new Post();
+                post.Content = Content;
+                post.Date = DateTime.Now;
+                post.Title = Title;
+
+                SelectedAuthor.Posts.Add(post);
+
+                authorDataRepository.UpdateAuthor(SelectedAuthor);
+
+                LoadPosts(SelectedAuthor);
+
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
         }
     }
 }
